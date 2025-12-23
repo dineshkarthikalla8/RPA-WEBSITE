07e74de73e77595ae00bde5f058008f75d11c006
Alla Dinesh Karthik <dineshkarthikalla@gmail.com>
2025-12-23 16:35:24 +0530

    Update teamData.jsx

--- src/data/teamData.jsx
+++ src/data/teamData.jsx
@@ -12,8 +12,8 @@ export const coordinators = [
   { name: "Manaswini", role: "Designer Lead", photo: "photos/MPP.jpg" },
   { name: "Akhila", role: "Designer Lead", photo: "photos/Akhila.jpg" },
   { name: "Sathya Naidu", role: "Technical Lead", photo: "photos/SN.jpg" },
-  { name: "Jaya Simha", role: "General Core", photo: "photos/Jay.jpeg" },
-  { name: "Aashrith", role: "General Core", photo: "photos/AAs.jpeg" },
+  { name: "Jaya Simha", role: "General Core", photo: "photos/Jay.jpg" },
+  { name: "Aashrith", role: "General Core", photo: "photos/AAs.jpg" },
 ]
 
 export const juniors = [
